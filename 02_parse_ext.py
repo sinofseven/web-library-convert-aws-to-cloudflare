067ddced7f06ea507c552a2deecaae9e4349aed9
sinofseven <em.s.00001@gmail.com>
2023-10-31 00:55:15 +0900

    fmt

--- 02_parse_ext.py
+++ 02_parse_ext.py
@@ -1,11 +1,9 @@
-from os.path import splitext, basename
-from sys import argv
 import json
+from os.path import basename, splitext
+from sys import argv
 
-
-with open('01_get_all_files.sh.txt') as f:
+with open("01_get_all_files.sh.txt") as f:
     result = set([splitext(x)[1] for x in f.read().split("\n") if x])
 
-with open(f'{basename(argv[0])}.json', 'w') as f:
+with open(f"{basename(argv[0])}.json", "w") as f:
     json.dump(sorted(result), f, indent=2, ensure_ascii=False)
-
