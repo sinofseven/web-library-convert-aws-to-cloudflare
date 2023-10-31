3a301d417950ee76497ad501cd24d04eeedbec96
sinofseven <em.s.00001@gmail.com>
2023-10-31 22:36:01 +0900

    upload

---
+++ 10_upload_raw_backup.py
@@ -0,0 +1,14 @@
+import boto3
+from os import getenv
+
+
+title = getenv("TITLE_SERIES")
+with open(f"{title}.zip", "rb") as f:
+    resp = boto3.client("s3").put_object(
+        Bucket="raw.backup.library.luciferous.link",
+        Key=f"{title}.zip",
+        Body=f,
+        StorageClass="GLACIER"
+    )
+print(resp)
+
