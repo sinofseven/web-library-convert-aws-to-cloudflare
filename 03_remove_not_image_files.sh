d8b4d2d188ca37cb76e41626205b7196a5b53bb7
sinofseven <em.s.00001@gmail.com>
2023-11-03 21:33:37 +0900

    fix ignore case

--- 03_remove_not_image_files.sh
+++ 03_remove_not_image_files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-find raw -type f | grep -v "jpg$" | grep -v "png$" | xargs rm -rf 
+find raw -type f | grep -i -v "jpg$" | grep -i -v "png$" | xargs rm -rf 
 find raw -type f -name ".*" | xargs rm -rf
 
