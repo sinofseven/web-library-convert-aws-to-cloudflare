510b6123e50b89172587e2eeb28912d942ccf313
sinofseven <em.s.00001@gmail.com>
2023-11-04 22:37:52 +0900

    add validate name

--- 05_create_convert_command.py
+++ 05_create_convert_command.py
@@ -3,15 +3,20 @@
 from os import makedirs
 from os.path import basename, dirname, splitext
 from sys import argv
+from re import Pattern, compile
 
 with open("04_get_image_list.sh.txt") as f:
     files = [x for x in f.read().split("\n") if x]
 
 
 dir_names = set()
+pattern: Pattern = compile(r"[^0-9a-zA-Z-_\/.]")
 result = []
 
 for path in files:
+    if pattern.search(path) is not None:
+        print(pattern.search(path))
+        raise ValueError(f"invalid charactor: path={path}")
     base = splitext(path[4:])[0]
     output = f"converted/{base}.avif"
     dir_names.add(dirname(output))
